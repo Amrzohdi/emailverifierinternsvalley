24f3ec3df75b9d84719dc83b36f6065e3645d5c8
Amr Essam <amr.essam67@gmail.com>
2019-03-15 15:28:48 +0200

    add rackattack file in initializers

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,13 @@
+Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new # defaults to Rails.cache
+
+
+Rack::Attack.throttle("requests by ip", limit: 5, period: 1.minutes) do |request|
+  if request.path == '/check' && request.get?
+    request.ip
+  end
+end
+
+
+Rack::Attack.throttled_response = lambda do |env|
+  [ 503, {}, ["retry after one minute."]]
+end
